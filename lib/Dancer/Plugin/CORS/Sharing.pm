a945de7f97f376716323fd49fffe041203254cf8
David Zurborg <zurborg@cpan.org>
2014-06-16 22:35:46 +0200

    add documentation

--- lib/Dancer/Plugin/CORS/Sharing.pm
+++ lib/Dancer/Plugin/CORS/Sharing.pm
@@ -1,18 +1,51 @@
 package Dancer::Plugin::CORS::Sharing;
 
+use Modern::Perl;
+use Scalar::Util qw(blessed);
+
+=head1 NAME
+
+Dancer::Plugin::CORS::Sharing - Helper class for I<sharing> keyword
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 DESCRIPTION
+
+...
+
+=head1 SYNOPSIS
+
+    use Dancer::Plugin::CORS;
+
+	sharing->rule(
+		origin => ...,
+		credentials => 1
+	);
+	
+	$route = post '/' => sub { ... };
+	
+	sharing->add($route);
+
+=head1 METHODS
+	
 =head2 new
 
 =cut
 
 sub new($%) {
 	my $class = shift;
+	if (blessed $class and !@_) {
+		$class->{rules} = [];
+		return $class;
+	}
 	my %options = (rules => [], @_);
 	return bless \%options => ref $class || $class;
-}
-
-sub _add_rule($$%) {
-	my ($self, $route, %options) = @_;
-	$self->{_add_rule}->($route, %options);
 }
 
 =head2 rule
@@ -24,36 +57,40 @@ sub rule($%) {
 	push @{$self->{rules}} => \%options;
 }
 
-=head2 addH
+=head2 add
 
 =cut
 
-sub addH($%) {
-	my ($self, %routes) = @_;
-	foreach my $routes (values %routes) {
-		$routes = [ $routes ] unless ref $routes eq 'ARRAY';
-		foreach my $route (@$routes) {
-			foreach my $options (@{$self->{rules}}) {
-				$self->_add_rule($route, %$options);
-			}
-		}
-	}
-}
-
-=head2 addA
-
-=cut
-
-sub addA($@) {
+sub add {
 	my ($self, @routes) = @_;
 	foreach my $routes (@routes) {
 		$routes = [ $routes ] unless ref $routes eq 'ARRAY';
 		foreach my $route (@$routes) {
 			foreach my $options (@{$self->{rules}}) {
-				$self->_add_rule($route, %$options);
+				$self->{_add_rule}->($route, %$options);
 			}
 		}
 	}
 }
+
+=head1 AUTHOR
+
+David Zurborg, C<< <zurborg@cpan.org> >>
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Dancer::Plugin::CORS>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2014 David Zurborg, all rights reserved.
+
+This program is released under the following license: open-source
+
+=cut
 
 1;
